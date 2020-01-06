620f3d8553e23ff9abd36e5c04a55e4187e1e0ca
nterol <n.terol@24sevres.com>
2020-01-06 16:11:59 +0100

    🎨 QueryWrapper HOC proposition

---
+++ src/components/QueryWrapper/QueryWrapper.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useQuery } from 'react-apollo';
+
+import RotationCircle from "../Loading/RotationCircle";
+
+const Error = () => <div>Error</div>
+
+function QueryWrapper({query, children,}) {
+    const {loading, data, error} = useQuery(query);
+
+    if (error) return <Error />
+    if (loading || !data) return <RotationCircle />
+    return children
+}
+
+export default QueryWrapper;
